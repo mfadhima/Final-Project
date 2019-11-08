ef51d0078fc679d0068561230717834eba995dd9
mfadhima <mfauzanadhima@hotmail.com>
2019-11-08 19:16:17 +0700

    multer

--- final-project-api/index.js
+++ final-project-api/index.js
@@ -2,18 +2,75 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const multer = require('multer')
 
 const {authRouter} = require('./2.routers/index')
+const db = require('./database/index')
 
 const port = 8888
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use('/files', express.static('productImage'))
 
 app.get(`/`, (req, res) => {
     res.send(`<h1> Final Project-ku </h1>`)
 })
 
 app.use('/auth', authRouter)
+
+
+// UPLOAD PRODUCT //
+let multerStorageConfig = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './productImage')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `PRODUCT-${Date.now()}.${file.mimetype.split('/')[1]}`)
+    }
+})
+
+let filterConfig = (req, file, cb) => {
+    if(file.mimetype.split('/')[1] == 'png' || file.mimetype.split('/')[1] == 'jpeg') {
+        cb(null, true)
+    } else {
+        req.validation = {
+            error : true,
+            msg : 'File must be an image'
+        }
+        cb(null, false)
+    }
+}
+
+let upload = multer(
+    {
+        storage : multerStorageConfig,
+        fileFilter : filterConfig
+    }
+)
+
+app.get('/products/productdata', (req, res) => {
+    db.query(`select * from products`, (err, result) => {
+        try {
+            if(err) throw err
+            res.send(result)
+        } catch (err) {
+            console.log(err)
+        }
+    })
+})
+
+app.post('/products/addproduct', upload.single('products'), (req, res) => {
+    db.query(`insert into products values(0, '${req.body.productName}', '${req.body.productDesc}', ${req.body.productPrice}, 'files/${req.file.filename}')`, (err, result) => {
+        try {
+            if(err) throw err
+            res.send(`Successfully add a product!`)
+        } catch (err) {
+            console.log(err)
+        }
+    })
+})
+// ============== //
+
 
 app.listen(port, console.log(`Listening to our favourite song.... (${port})`))
--- final-project/src/components/ManageProducts.jsx
+++ final-project/src/components/ManageProducts.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
+import Axios from 'axios'
 
 import './ManageProducts.css'
 
+const URL_API = 'http://localhost:8888/'
+
 class ManageProducts extends Component {
+    state = {
+        productName: '',
+        productDesc: '',
+        productPrice: 0,
+        productImage: null,
+        productData: [],
+        message: ''
+    }
+
+    onAddClick = () => {
+        if(!this.state.productName || !this.state.productDesc || !this.state.productPrice || !this.state.productImage) {
+            this.setState({message: 'Please fill all forms..'})
+        } else {
+            let fd = new FormData()
+            fd.append('products', this.state.productImage, this.state.productImage.name)
+            fd.append('productName', this.state.productName)
+            fd.append('productDesc', this.state.productDesc)
+            fd.append('productPrice', this.state.productPrice)
+            Axios.post(
+                URL_API + 'products/addproduct', fd
+            ).then((res) => {
+                console.log(res)
+                this.setState({message: res})
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+
     render() {
         if(this.props.role === 'administrator') {
             return(
@@ -13,28 +45,36 @@ class ManageProducts extends Component {
                     <div className="container">
                         <h5 className="mt-3">Add new Product</h5>
                         <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Product Name</th>
-                                    <th>Description</th>
-                                    <th>Price</th>
-                                    <th>Image</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
                             <tbody>
                                 <tr>
-                                    <td><input type="text" className="form-control"/></td>
-                                    <td><input type="text" className="form-control"/></td>
-                                    <td><input type="text" className="form-control"/></td>
+                                    <td><input required type="text" className="form-control" onChange={(e) => {this.setState({productName: e})}} placeholder="Product Name"/></td>
+                                    <td><input required type="text" className="form-control" onChange={(e) => {this.setState({productDesc: e})}} placeholder="Description"/></td>
+                                    <td><input required type="text" className="form-control" onChange={(e) => {this.setState({productPrice: e})}} placeholder="Price"/></td>
                                     <td>
-                                        <input type="file" hidden/>
-                                        <button className="button-ku">Choose an image</button>
+                                        <input
+                                            type="file"
+                                            accept="image/*"
+                                            ref="uploadImage"
+                                            hidden
+                                            onChange={(e) => {this.setState({productImage: e.target.files[0]})}}
+                                        />
+                                        <input
+                                            type="button"
+                                            value="Choose an image"
+                                            className="button-ku"
+                                            onClick={() => {this.refs.uploadImage.click()}}
+                                        />
                                     </td>
-                                    <td><button className="button-ku">Add Product</button></td>
+                                    <td><button className="button-ku" onClick={this.onAddClick}>Add Product</button></td>
                                 </tr>
                             </tbody>
                         </table>
+
+                        <h5>Our Products</h5>
+                        <table className="table">
+
+                        </table>
+
                     </div>
                 </div>
             )
